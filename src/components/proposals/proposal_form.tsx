cebdee568c03cd5e12d7a1effcd3e71ee4a09db2
hidenoriwakiri <rtuocck2.020.hi@gmail.com>
2022-11-06 08:06:55 +0900

    MOD: proposal input form

--- src/components/proposals/proposal_form.tsx
+++ src/components/proposals/proposal_form.tsx
@@ -25,27 +25,17 @@ const FormView = (form: Form<Proposal>): JSX.Element => {
   };
   return (
     <AntdForm {...layout}>
-      <InputField label="タイトル" form={form} attr="title" />
-      <SelectField
-        form={form}
-        attr="status"
-        label="ステータス"
-        selectItems={ProposalStatusList.map((s) => ({
-          label: s,
-          value: s,
-        }))}
-      />
-      <DateField label="投票期日" form={form} attr="endDate" />
-      <TextAreaField label="トランザクション" form={form} attr="transaction" />
+      <TextAreaField label="タイトル" form={form} attr="title" />
+      <TextAreaField label="コントラクトアドレス" form={form} attr="targets" />
+      <InputField type="number" label="値" form={form} attr="values" />
+      <TextAreaField label="関数シグネチャ" form={form} attr="signatures" />
+      <TextAreaField label="データ" form={form} attr="datas" />
+      <TextAreaField label="データ型" form={form} attr="datatypes" />
       <TextAreaField label="詳細" form={form} attr="description" />
-      <InputField type="number" label="定足数" form={form} attr="quorum" />
-      <InputField type="number" label="賛成数" form={form} attr="forCount" />
-      <InputField
-        type="number"
-        label="反対数"
-        form={form}
-        attr="againstCount"
-      />
+      {/*
+        temp
+      */}
+      <TextAreaField label="状態" form={form} attr="status" />
     </AntdForm>
   );
 };
